f61d07dc648ba681542707f4328282285168870b
TimurMP <titimurmamyshev@gmail.commur>
2022-07-07 00:45:56 +0300

    >>

--- src/telran/array/tools/MemoryService.java
+++ src/telran/array/tools/MemoryService.java
@@ -1,2 +1,62 @@
-package telran.array.tools;public class MemoryService {
+package telran.array.tools;
+
+public class MemoryService {
+    public static int getMaxArrayMemory(){
+        int [] arr;
+
+
+        int l = 0;
+        int r = Integer.MAX_VALUE;
+        int mid = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if (l+r > 0){
+                mid = (-(l+r)-2)/2;
+
+            }
+            if (l+r<0){
+                mid = l+r/2;
+            }
+            System.out.println("mid" + mid);
+            try {
+                arr = new int[mid];
+                l = mid+1;
+                System.out.println(mid);
+
+
+            } catch (OutOfMemoryError e) {
+                r = mid-1;
+                l = mid + mid/2;
+
+
+            }
+
+        }
+
+
+
+
+//        for (int i = 0; i < 32; i++) {
+//            int temp = 0;
+//            temp = temp+(arrSize/2);
+//
+//            System.out.println(i);
+//            try {
+//
+//                arr = new int[temp];
+//                arrSize = arrSize/2;
+//
+//
+//                System.out.println(temp);
+//
+//
+//            }catch (OutOfMemoryError e){
+//
+//                arrSize = arrSize/2;
+//                System.out.println("VM limit");
+//            }
+//
+//        }
+        return mid-1;
+    }
 }
--- src/telran/array/tools/MemoryServiceTest.java
+++ src/telran/array/tools/MemoryServiceTest.java
@@ -1,4 +1,53 @@
+package telran.array.tools;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MemoryServiceTest {
-  
-}
+    int [] arr;
+
+    @Test
+    void getMaxArrayMemory() {
+
+        int maxSize = MemoryService.getMaxArrayMemory();
+        boolean flag;
+        try {
+            arr = new int[maxSize];
+            flag = true;
+            arr = null;
+
+        }catch (OutOfMemoryError e){
+            flag = false;
+        }
+        assertTrue(flag);
+
+        try {
+            arr = new int[maxSize+1];
+            flag = true;
+            arr = null;
+
+        }catch (OutOfMemoryError e){
+            flag = false;
+        }
+        assertFalse(flag);
+    }
+
+
+    @Test
+    void temp() {
+        int arrSize = 100;
+        int temp = 100;
+        arrSize = arrSize / 2;
+        temp = arrSize/2;
+        System.out.println(temp);
+        arrSize = arrSize + temp;
+
+        System.out.println(arrSize);
+
+
+
+
+    }
+
+    }
